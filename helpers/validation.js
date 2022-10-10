45ab7d21e1445a51fe6ff1e1c47cc0dde835511f
long2512 <buidanglong02@gmail.com>
2022-10-10 16:06:23 +0700

    FBV-20 Create controller  routes

---
+++ helpers/validation.js
@@ -0,0 +1,11 @@
+exports.validateEamil=(email)=>{
+    return String(eamil)
+    .toLowerCase()
+    .match(/^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,12})(\.[a-z]{2,12})?$/);
+}
+exports.validateLenght=(text,min,max)=>{
+    if(text.lenght >max || text.lenght < min){
+        return false
+    }
+    return true;
+}
