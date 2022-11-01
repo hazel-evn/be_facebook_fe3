8240c1dff2b1db323bd8dcc0c5ae1ebc128416c4
long2512 <buidanglong02@gmail.com>
2022-11-01 16:58:52 +0700

    Feat; delete post

--- controllers/post.js
+++ controllers/post.js
@@ -38,3 +38,12 @@ exports.getAllPosts = async (req, res) => {
 };
 
 
+
+exports.deletePost = async (req, res) => {
+  try {
+    await Post.findByIdAndRemove(req.params.id);
+    res.json({ status: "ok" });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
