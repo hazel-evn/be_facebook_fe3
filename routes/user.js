cedae8971a51f0f69b857f31612ffb0f29b4b920
long2512 <buidanglong02@gmail.com>
2022-11-02 13:24:31 +0700

    Fix route user

--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,8 @@ const { register, login, activateAccount} = require("../controllers/user");
 
 const router = express.Router();
 
+const { authUser } = require("../middlwares/auth");
+
 router.post("/register",register);
 router.post("/activate", authUser, activateAccount);
 router.post("/login",login);
