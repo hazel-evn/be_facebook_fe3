4722713f289cea3eacab3ea9ffb2d8597cc7c246
long2512 <buidanglong02@gmail.com>
2022-11-12 13:10:58 +0700

    Fix route user

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const { register, login, activateAccount} = require("../controllers/user");
+const { register, login, activateAccount, findUser, sendResetPasswordCode, validateResetCode, changePassword, getProfile, updateProfilePicture, updateCover, updateDetails, addFriend} = require("../controllers/user");
 
 const router = express.Router();
 
@@ -9,5 +9,14 @@ const { authUser } = require("../middlwares/auth");
 router.post("/register",register);
 router.post("/activate", authUser, activateAccount);
 router.post("/login",login);
+router.post("/findUser", findUser);
+router.post("/sendResetPasswordCode", sendResetPasswordCode);
+router.post("/validateResetCode", validateResetCode);
+router.post("/changePassword", changePassword);
+router.get("/getProfile/:username", authUser, getProfile);
+router.put("/updateProfilePicture", authUser, updateProfilePicture);
+router.put("/updateCover", authUser, updateCover);
+router.put("/updateDetails", authUser, updateDetails);
+router.put("/addFriend/:id", authUser, addFriend);
 
 module.exports = router;
